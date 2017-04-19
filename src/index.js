f835be30ba7ec424610684c703c624b32fa5f694
Richard Littauer <richard.littauer@gmail.com>
2017-04-19 19:59:15 -0400

    Call data instead of loading it locally

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const L = require('leaflet')
 const domready = require('domready')
+var $ = require('jquery')
 
 const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
 
@@ -12,6 +13,13 @@ domready(function () {
     accessToken: MAPBOX_ACCESS_TOKEN
   }).addTo(mymap)
 
-  // Test marker
-  var marker = L.marker([0.0236, 37.9062]).addTo(mymap)
+  var geojsonURL = 'https://opendata.arcgis.com/datasets/73579b946adb446186e29702276aa77a_0.geojson'
+
+  $.getJSON(geojsonURL, function (neighbourhoods) {
+    L.geoJson(neighbourhoods, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.NBarri)
+      }
+    }).addTo(mymap)
+  })
 })
