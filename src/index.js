e122b871b8e8aa81d99f79b672d74f11b6bfce9f
Richard Littauer <richard.littauer@gmail.com>
2017-04-19 19:05:06 -0400

    Adding initial map and test marker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const L = require('leaflet')
 const domready = require('domready')
 
-// Good, this is totally being called.
-console.log('hi')
-console.log(leaflet)
+const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
+
+domready(function () {
+  const mymap = L.map('mapid').setView([0.0236, 37.9062], 13)
+  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=' + MAPBOX_ACCESS_TOKEN, {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 7,
+    id: 'mapbox.mapbox-streets-v7',
+    accessToken: MAPBOX_ACCESS_TOKEN
+  }).addTo(mymap)
+
+  // Test marker
+  var marker = L.marker([0.0236, 37.9062]).addTo(mymap)
+})
